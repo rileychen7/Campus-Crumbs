71836c6e4d9355a28d3083ffdf161634c34dde20
Johnny Chen <149898820+Johnnynyc@users.noreply.github.com>
2023-11-04 17:45:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,4 @@
-from PIL import Image
-import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
-
-st.set_page_config(page_title="StudiFood", page_icon="✅")
-
-st.title("StudiFood")
-
-st.sidebar.header("Navigation")
-page = st.sidebar.selectbox("Select a page", ['Page 1', 'Page 2', 'Page 3'])
-
-def load_lottieur(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-if page == 'Page 1':
-    st.header("Welcome to StudiFood!")
-    st.write("Order delicious campus food and have it delivered to your dorm.")
-
-    lottie_coding = load_lottieur("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
-
-    with st.columns(2):
-        st_lottie(lottie_coding, height=300, key="coding")
-
 # Your code for other pages...
 
 if page == 'Page 2':
