b20803934657ed4d58dc7e76cd27708de168fee8
Johnny Chen <149898820+Johnnynyc@users.noreply.github.com>
2023-11-04 17:49:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,22 +28,22 @@ if page == 'Page 2':
     col2.write("At Au Bon Pain in Greiner Hall, we take our service - and menu - From our scrumptious pastries and premium coffee line to inspired menus filled with savory sandwiches, soups and salads, our lively, bustling marketplace allows you to personally select the freshest choices.")
 
     col1, col2 = st.columns([1, 3])  
-    col1.image("Hubies.png", use_column_width=True)
+    col1.image("hubies.png", use_column_width=True)
     col2.subheader("Hubies")
     col2.write("Hubies in the Ellicott Food Court has a little of everything. Hot, fresh pizza? - you bet - it's a UB tradition. And nobody does wings, fingers and subs like Hubies.")
 
     col1, col2 = st.columns([1, 3])  
-    col1.image("Wrapitup.png", use_column_width=True)
+    col1.image("wrapitup.png", use_column_width=True)
     col2.subheader("Wrap it up")
     col2.write("Make mine a wrap! It will be hard to choose from all the varieties of wraps we offer at Wrap it Up in the Ellicott Food Court. Whether it's filled with meat, eggs, vegetables or even fruit, we've got a wrap variety that will soon become your favorite. As a side, we offer hearty soups and salted snacks. This is also the place for bagels; top one with butter, one of our flavored cream cheeses, or add meat and cheese to make a sandwich.")
 
     col1, col2 = st.columns([1, 3])  
-    col1.image("Sizzles.png", use_column_width=True)
+    col1.image("sizzles.png", use_column_width=True)
     col2.subheader("Sizzles")
     col2.write("What a grill we've got! Meet us at Sizzles in the Ellicott Food Court for breakfast and try our fresh eggs, sausage, bacon and hash browns. Our lunch and dinner fare features hot, grilled sandwiches, made just how you want them. So for all things grilled, this is the place to go.")
 
     col1, col2 = st.columns([1, 3])  
-    col1.image("The Bowl.png", use_column_width=True)
+    col1.image("thebowl.png", use_column_width=True)
     col2.subheader("The Bowl")
     col2.write("The Bowl in the Ellicott Food Court is the place to go when you're thinking lighter or maybe some comforting soup. Our salad bar offers a tremendous variety; the freshest veggies, an assortment of greens, cheeses, crispy toppings, specialty oils and vinegars, and dressings.")
 
@@ -68,7 +68,7 @@ if page == 'Page 2':
     col1.image("jamba.png", use_column_width=True)
     col2.subheader("Jamba")
     col2.write("Stop in and enjoy the world's freshest, most fruit-filling experience. Jamba in the Student Union has a wide variety for smoothies, fruit juices and so much more.")
-
+    
     for item in menu_items:
         col1, col2 = st.columns([1, 3])
         col1.image(item["image"], use_column_width=True)
