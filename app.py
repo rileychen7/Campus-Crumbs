3adb96a1adce84ffa9901e357cfe150bce8c781f
Johnny Chen <149898820+Johnnynyc@users.noreply.github.com>
2023-11-04 16:43:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,23 +13,14 @@ if page == 'Page 1':
     st.header("Welcome to StudiFood!")
     st.write("Order delicious campus food and have it delivered to your dorm.")
 
-    def load_lottie(url: str):
-        try:
-            request = requests.get(url)
-            request.raise_for_status()
-            return request.json()
-        except requests.exceptions.RequestException as e:
-            st.error(f"Failed to load Lottie animation: {str(e)}")
-            return None
-        except ValueError as e:
-            st.error(f"Invalid Lottie animation JSON: {str(e)}")
-            return None
+    def load_lottieur(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
-    lottie_url = "https://app.lottiefiles.com/animation/f00039ae-d989-4d4c-9ae8-cad4f9e25570"
-    lottie = load_lottie(lottie_url)
-
-    if lottie:
-        st_lottie(lottie, height=300, key="delivery")
+    lottie_coding = load_lottieur("https://app.lottiefiles.com/animation/f00039ae-d989-4d4c-9ae8-cad4f9e25570")
+    st_lottie(lottie_coding)
 
 if page == 'Page 2':
     st.header("Menu Selection")
