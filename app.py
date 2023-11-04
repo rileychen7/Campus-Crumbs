291bb5e01e2359997cf5f6f0a6d1a387bc80b969
Riley Chen <144640158+rileychen7@users.noreply.github.com>
2023-11-04 16:20:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import requests
 import streamlit as st
+import requests
 from streamlit_lottie import st_lottie
-
 
 st.set_page_config(page_title="StudiFood", page_icon="✅")
 
@@ -10,13 +9,13 @@ st.title("StudiFood")
 st.sidebar.header("Navigation")
 page = st.sidebar.selectbox("Select a page", ['Page 1', 'Page 2', 'Page 3'])
 
-#Animation
+# Animation
 def load_lottie(url):
-    request = requests.get(url)
-    if request.status_code != 200:
+    response = requests.get(url)
+    if response.status_code != 200:
         return None
-    return request.json()
-    
+    return response.json()
+
 lottie = load_lottie("https://app.lottiefiles.com/animation/f00039ae-d989-4d4c-9ae8-cad4f9e25570")
 
 st_lottie(lottie, height=300, key="delivery")
@@ -40,60 +39,7 @@ if page == 'Page 2':
     col2.subheader("Champa Sushi")
     col2.write("Order Champa Sushi in the Student Union for some fresh sushi!")
 
-    col1, col2 = st.columns([1, 3])  
-    col1.image("jamba.png", use_column_width=True)
-    col2.subheader("Jamba")
-    col2.write("Stop in and enjoy the world's freshest, most fruit-filling experience. Jamba in the Student Union has a wide variety for smoothies, fruit juices and so much more.")
-
-    col1, col2 = st.columns([1, 3])  
-    col1.image("moes.png", use_column_width=True)
-    col2.subheader("Moe's")
-    col2.write("Moe’s Southwest Grill in the Student Union is a fun and engaging fast-casual concept serving a wide variety of fresh, made-to-order southwest fare")
-    
-    col1, col2 = st.columns([1, 3])  
-    col1.image("pistachios.png", use_column_width=True)
-    col2.subheader("Pistachio's")
-    col2.write(""" Pistachio's is the perfect place to go for delicious pasta dishes from Bravo Pasta or a hot panini from the Bread Box Deli.
-At Bravo Pasta, our culinary team is blending the freshest and finest quality ingredients to make hearty pasta dishes!""")
-
-    col1, col2 = st.columns([1, 3])  
-    col1.image("tims.png", use_column_width=True)
-    col2.subheader("Tim Hortons")
-    col2.write("Hot, fresh coffee and delicious baked goods. Serving a variety of muffins, bagels, doughnuts and more!")
-    
-    col1, col2 = st.columns([1, 3])  
-    col1.image("kali.png", use_column_width=True)
-    col2.subheader("Kali Orexi ")
-    col2.write("Mediterranean cuisine and fare from Middle Eastern countries. Here you will find marinated choice cuts of meats, ancient grains and regional spices.")
-
-    col1, col2 = st.columns([1, 3])  
-    col1.image("tikka.png", use_column_width=True)
-    col2.subheader("Tikka Table")
-    col2.write("Traditional dishes with flavorful spices from the different regions of India.")
-
-    col1, col2 = st.columns([1, 3])  
-    col1.image("1846.png", use_column_width=True)
-    col2.subheader("1846 Grill")
-    col2.write("Classic American comfort food – from breakfast to burgers and everything in between.")
-
-    col1, col2 = st.columns([1, 3])  
-    col1.image("panasia.png", use_column_width=True)
-    col2.subheader("Champa Sushi")
-    col2.write("Flavors from China, Korea and Taiwan abound in a variety of dishes on a rotating basis.")
-
-    col1, col2 = st.columns([1, 3])  
-    col1.image("noodle.png", use_column_width=True)
-    col2.subheader("Noodle Pavillion")
-    col2.write("Popular Japanese and Vietnamese bowl-style cuisine with a wide selection of fresh options.")
-
+    # Rest of your code for Page 2...
 
 if page == 'Page 3':
-    st.header("Order History")
-    st.write("View your previous orders and their status")
-
-st.markdown(
-    """
-    * * *
-    StudiFood - Developed from UB Hacking 2023
-    """
-)
+    st.header("
