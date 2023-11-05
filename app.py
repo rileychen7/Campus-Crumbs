c585208327210a4c5e401b3c21711ee86a4e2bdf
Animesh Saha <58145811+animeshsaha0906@users.noreply.github.com>
2023-11-04 22:41:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -54,9 +54,8 @@ if st.session_state.page == 'Home Page':
                 "Fruit Cup": "$3.99",
                 "Yogurt": "$2.49"
             }
-            plus=st.button("+")
             for item, price in menu_items.items():
-                col2.write(f"- {item}: {price} {plus}")
+                col2.write(f"- {item}: {price}")
 
 
         elif restaurant_ellicott == "Au Bon Pain":
