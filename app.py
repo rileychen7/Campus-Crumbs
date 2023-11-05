dee2b27ffcddbe45676accdd060683994ecbad44
Animesh Saha <58145811+animeshsaha0906@users.noreply.github.com>
2023-11-04 23:02:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,136 @@ if st.session_state.page == 'Home Page':
             }
             for item, price in menu_items.items():
                 col2.write(f"- {item}: {price}")
-        elif restaurant_ellicot=="Show All Restaurant":
+        
+        elif restaurant_ellicott == "Au Bon Pain":
+            col1, col2 = st.columns([1, 3])  
+            col1.image("aubonpain.png", use_column_width=True)
+            col2.subheader("au bon pain The Bakery Cafe")
+            col2.write("At Au Bon Pain in Greiner Hall, we take our service - and menu - From our scrumptious pastries and premium coffee line to inspired menus filled with savory sandwiches, soups and salads, our lively, bustling marketplace allows you to personally select the freshest choices.")
+            col2.subheader("Menu")
+            menu_items = {
+                "Croissant": "$2.99",
+                "Cappuccino": "$3.49",
+                "Turkey & Swiss Sandwich": "$7.99",
+                "Chicken Caesar Salad": "$6.99",
+                "Muffin": "$2.49",
+                "Fruit Salad": "$4.99"
+            }
+
+            for item, price in menu_items.items():
+                col2.write(f"- {item}: {price}")
+                
+        elif restaurant_ellicott == "Hubies":
+            col1, col2 = st.columns([1, 3])  
+            col1.image("hubies.png", use_column_width=True)
+            col2.subheader("Hubies")
+            col2.write("Hubies in the Ellicott Food Court has a little of everything. Hot, fresh pizza? - you bet - it's a UB tradition. And nobody does wings, fingers and subs like Hubies.")
+            col2.subheader("Menu")
+            menu_items = {
+                "Cheese Pizza": "$10.99",
+                "Pepperoni Pizza": "$11.99",
+                "Buffalo Chicken Wings (10 pcs)": "$9.99",
+                "Chicken Tenders (6 pcs)": "$7.99",
+                "Philly Cheesesteak Sub": "$8.99",
+                "Vegetarian Sub": "$7.99"
+            }
+
+            for item, price in menu_items.items():
+                col2.write(f"- {item}: {price}")
+
+            
+        elif restaurant_ellicott == "Wrap it Up":
+            col1, col2 = st.columns([1, 3])  
+            col1.image("wrapitup.png", use_column_width=True)
+            col2.subheader("Wrap it up")
+            col2.write("Make mine a wrap! It will be hard to choose from all the varieties of wraps we offer at Wrap it Up in the Ellicott Food Court. Whether it's filled with meat, eggs, vegetables or even fruit, we've got a wrap variety that will soon become your favorite. As a side, we offer hearty soups and salted snacks. This is also the place for bagels; top one with butter, one of our flavored cream cheeses, or add meat and cheese to make a sandwich.")
+            col2.subheader("Menu")
+            menu_items = {
+                "Chicken Caesar Wrap": "$8.99",
+                "Veggie Wrap": "$7.99",
+                "Fruit Salad": "$3.99",
+                "Soup of the Day": "$4.99",
+                "Bagel with Cream Cheese": "$2.99",
+                "Turkey Club Wrap": "$9.99"
+            }
+
+            for item, price in menu_items.items():
+                col2.write(f"- {item}: {price}")
+
+            
+        elif restaurant_ellicott == "Sizzles":
+            col1, col2 = st.columns([1, 3])  
+            col1.image("sizzles.png", use_column_width=True)
+            col2.subheader("Sizzles")
+            col2.write("What a grill we've got! Meet us at Sizzles in the Ellicott Food Court for breakfast and try our fresh eggs, sausage, bacon and hash browns. Our lunch and dinner fare features hot, grilled sandwiches, made just how you want them. So for all things grilled, this is the place to go.")
+            col2.subheader("Menu")
+            menu_items = {
+                "Bacon, Egg & Cheese Breakfast Sandwich": "$4.99",
+                "Grilled Chicken Panini": "$8.99",
+                "Classic Cheeseburger": "$6.99",
+                "Veggie Wrap": "$7.99",
+                "French Fries": "$2.99",
+                "Milkshake": "$3.99"
+            }
+
+            for item, price in menu_items.items():
+                col2.write(f"- {item}: {price}")
+
+            
+        elif restaurant_ellicott == "The Bowl":
+            col1, col2 = st.columns([1, 3])  
+            col1.image("thebowl.png", use_column_width=True)
+            col2.subheader("The Bowl")
+            col2.write("The Bowl in the Ellicott Food Court is the place to go when you're thinking lighter or maybe some comforting soup. Our salad bar offers a tremendous variety; the freshest veggies, an assortment of greens, cheeses, crispy toppings, specialty oils and vinegars, and dressings.")
+            col2.subheader("Menu")
+            menu_items = {
+                "Create Your Own Salad": "Price varies by ingredients",
+                "Soup of the Day": "$4.99",
+                "Crispy Chicken Salad": "$8.99",
+                "Greek Salad": "$7.99",
+                "Caesar Salad": "$6.99",
+                "Fruit Salad": "$3.99"
+            }
+
+            for item, price in menu_items.items():
+                col2.write(f"- {item}: {price}")
+            
+        elif restaurant_ellicott == "Guac and Roll":
+            col1, col2 = st.columns([1, 3])  
+            col1.image("guacandroll.png", use_column_width=True)
+            col2.subheader("Guac and Roll")
+            col2.write("If authentic Mexican food is what you're craving, stop by Guac and Roll in the Ellicott Food Court. Our menu features traditional Mexican favorites — tacos, burritos, nachos and more — with a wide variety of sides to accompany your main dish. Choose from one of our homemade salsas and guacamole. We've got everything to give you that South of the Border eating experience.")
+            col2.subheader("Menu")
+            menu_items = {
+                "Taco (Beef or Chicken)": "$3.99",
+                "Burrito (Vegetarian or Pork)": "$7.99",
+                "Nachos with Cheese": "$4.99",
+                "Chips & Guacamole": "$5.99",
+                "Quesadilla": "$6.99",
+                "Mexican Rice": "$2.99"
+            }
+
+            for item, price in menu_items.items():
+                col2.write(f"- {item}: {price}")
+            
+        elif restaurant_ellicott == "Perks":
+            col1, col2 = st.columns([1, 3])  
+            col1.image("perks.png", use_column_width=True)
+            col2.subheader("Perks")
+            col2.write("Perks in the Ellicott Food Court is so much more than your average neighborhood coffee house. Comfortable couches and our cool blues and jazz motif sets the mood for sipping high quality, fresh roasted coffees and teas. Choose from a regular cup of joe or tea, to espresso and specialty iced coffees. Every day we offer a variety of different brew blends from a light roast to a darker roast. A freshly baked muffin, cookie or scone is the perfect compliment to your hot beverage. The Ice Cream Shoppe offers delightfully refreshing treats with sundaes, milkshakes and cones.")
+            col2.subheader("Menu")
+            menu_items = {
+                "Latte": "$4.49",
+                "Iced Coffee": "$3.99",
+                "Muffin": "$2.99",
+                "Cappuccino": "$4.99",
+                "Cookie": "$1.99",
+                "Ice Cream Sundae": "$5.99"
+            }
+
+            for item, price in menu_items.items():
+                col2.write(f"- {item}: {price}")
+    elif delivery_location=="Show All Restaurant":
             st.header("Menu Selection")
             st.write("Browse the menu and select your favorite items")
         
@@ -187,136 +316,7 @@ if st.session_state.page == 'Home Page':
             col2.subheader("Teddy's")
             col2.write("Teddy's is our convenience store in Governors!")
                     
-
-        elif restaurant_ellicott == "Au Bon Pain":
-            col1, col2 = st.columns([1, 3])  
-            col1.image("aubonpain.png", use_column_width=True)
-            col2.subheader("au bon pain The Bakery Cafe")
-            col2.write("At Au Bon Pain in Greiner Hall, we take our service - and menu - From our scrumptious pastries and premium coffee line to inspired menus filled with savory sandwiches, soups and salads, our lively, bustling marketplace allows you to personally select the freshest choices.")
-            col2.subheader("Menu")
-            menu_items = {
-                "Croissant": "$2.99",
-                "Cappuccino": "$3.49",
-                "Turkey & Swiss Sandwich": "$7.99",
-                "Chicken Caesar Salad": "$6.99",
-                "Muffin": "$2.49",
-                "Fruit Salad": "$4.99"
-            }
-
-            for item, price in menu_items.items():
-                col2.write(f"- {item}: {price}")
-                
-        elif restaurant_ellicott == "Hubies":
-            col1, col2 = st.columns([1, 3])  
-            col1.image("hubies.png", use_column_width=True)
-            col2.subheader("Hubies")
-            col2.write("Hubies in the Ellicott Food Court has a little of everything. Hot, fresh pizza? - you bet - it's a UB tradition. And nobody does wings, fingers and subs like Hubies.")
-            col2.subheader("Menu")
-            menu_items = {
-                "Cheese Pizza": "$10.99",
-                "Pepperoni Pizza": "$11.99",
-                "Buffalo Chicken Wings (10 pcs)": "$9.99",
-                "Chicken Tenders (6 pcs)": "$7.99",
-                "Philly Cheesesteak Sub": "$8.99",
-                "Vegetarian Sub": "$7.99"
-            }
-
-            for item, price in menu_items.items():
-                col2.write(f"- {item}: {price}")
-
-            
-        elif restaurant_ellicott == "Wrap it Up":
-            col1, col2 = st.columns([1, 3])  
-            col1.image("wrapitup.png", use_column_width=True)
-            col2.subheader("Wrap it up")
-            col2.write("Make mine a wrap! It will be hard to choose from all the varieties of wraps we offer at Wrap it Up in the Ellicott Food Court. Whether it's filled with meat, eggs, vegetables or even fruit, we've got a wrap variety that will soon become your favorite. As a side, we offer hearty soups and salted snacks. This is also the place for bagels; top one with butter, one of our flavored cream cheeses, or add meat and cheese to make a sandwich.")
-            col2.subheader("Menu")
-            menu_items = {
-                "Chicken Caesar Wrap": "$8.99",
-                "Veggie Wrap": "$7.99",
-                "Fruit Salad": "$3.99",
-                "Soup of the Day": "$4.99",
-                "Bagel with Cream Cheese": "$2.99",
-                "Turkey Club Wrap": "$9.99"
-            }
-
-            for item, price in menu_items.items():
-                col2.write(f"- {item}: {price}")
-
-            
-        elif restaurant_ellicott == "Sizzles":
-            col1, col2 = st.columns([1, 3])  
-            col1.image("sizzles.png", use_column_width=True)
-            col2.subheader("Sizzles")
-            col2.write("What a grill we've got! Meet us at Sizzles in the Ellicott Food Court for breakfast and try our fresh eggs, sausage, bacon and hash browns. Our lunch and dinner fare features hot, grilled sandwiches, made just how you want them. So for all things grilled, this is the place to go.")
-            col2.subheader("Menu")
-            menu_items = {
-                "Bacon, Egg & Cheese Breakfast Sandwich": "$4.99",
-                "Grilled Chicken Panini": "$8.99",
-                "Classic Cheeseburger": "$6.99",
-                "Veggie Wrap": "$7.99",
-                "French Fries": "$2.99",
-                "Milkshake": "$3.99"
-            }
-
-            for item, price in menu_items.items():
-                col2.write(f"- {item}: {price}")
-
-            
-        elif restaurant_ellicott == "The Bowl":
-            col1, col2 = st.columns([1, 3])  
-            col1.image("thebowl.png", use_column_width=True)
-            col2.subheader("The Bowl")
-            col2.write("The Bowl in the Ellicott Food Court is the place to go when you're thinking lighter or maybe some comforting soup. Our salad bar offers a tremendous variety; the freshest veggies, an assortment of greens, cheeses, crispy toppings, specialty oils and vinegars, and dressings.")
-            col2.subheader("Menu")
-            menu_items = {
-                "Create Your Own Salad": "Price varies by ingredients",
-                "Soup of the Day": "$4.99",
-                "Crispy Chicken Salad": "$8.99",
-                "Greek Salad": "$7.99",
-                "Caesar Salad": "$6.99",
-                "Fruit Salad": "$3.99"
-            }
-
-            for item, price in menu_items.items():
-                col2.write(f"- {item}: {price}")
-            
-        elif restaurant_ellicott == "Guac and Roll":
-            col1, col2 = st.columns([1, 3])  
-            col1.image("guacandroll.png", use_column_width=True)
-            col2.subheader("Guac and Roll")
-            col2.write("If authentic Mexican food is what you're craving, stop by Guac and Roll in the Ellicott Food Court. Our menu features traditional Mexican favorites — tacos, burritos, nachos and more — with a wide variety of sides to accompany your main dish. Choose from one of our homemade salsas and guacamole. We've got everything to give you that South of the Border eating experience.")
-            col2.subheader("Menu")
-            menu_items = {
-                "Taco (Beef or Chicken)": "$3.99",
-                "Burrito (Vegetarian or Pork)": "$7.99",
-                "Nachos with Cheese": "$4.99",
-                "Chips & Guacamole": "$5.99",
-                "Quesadilla": "$6.99",
-                "Mexican Rice": "$2.99"
-            }
-
-            for item, price in menu_items.items():
-                col2.write(f"- {item}: {price}")
-            
-        elif restaurant_ellicott == "Perks":
-            col1, col2 = st.columns([1, 3])  
-            col1.image("perks.png", use_column_width=True)
-            col2.subheader("Perks")
-            col2.write("Perks in the Ellicott Food Court is so much more than your average neighborhood coffee house. Comfortable couches and our cool blues and jazz motif sets the mood for sipping high quality, fresh roasted coffees and teas. Choose from a regular cup of joe or tea, to espresso and specialty iced coffees. Every day we offer a variety of different brew blends from a light roast to a darker roast. A freshly baked muffin, cookie or scone is the perfect compliment to your hot beverage. The Ice Cream Shoppe offers delightfully refreshing treats with sundaes, milkshakes and cones.")
-            col2.subheader("Menu")
-            menu_items = {
-                "Latte": "$4.49",
-                "Iced Coffee": "$3.99",
-                "Muffin": "$2.99",
-                "Cappuccino": "$4.99",
-                "Cookie": "$1.99",
-                "Ice Cream Sundae": "$5.99"
-            }
-
-            for item, price in menu_items.items():
-                col2.write(f"- {item}: {price}")
-            
+   
     if delivery_location == 'North Campus Academic Buildings':
         st.markdown("You've selected delivery to North Campus Academic Buildings.")
         restaurant_north = st.selectbox("Select a restaurant from North Campus Academic Buildings", ['Champa', 'Jamba', 'Moes', 'Pistachios', 'Tims', 'Kali', 'Tikka', '1846 Grill', 'Pan Asia', 'Noodle Pavilion'])
