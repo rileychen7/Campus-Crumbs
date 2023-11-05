ff172869c6488d887f52f2208cff4846ab85b3c5
Johnny Chen <149898820+Johnnynyc@users.noreply.github.com>
2023-11-05 04:40:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -54,18 +54,16 @@ if st.session_state.page == 'Order Now':
             }
             menu_elli = st.selectbox("Select item from menu", ['Chips', 'Soda', 'Sandwich', 'Salad', 'Fruit Cup', 'Yogurt'])
             count = 0
-            value=0
-            add_item=st.button("ADD")
-            if add_item:
-                count+=1
-                menu_items["Chips"]=menu_items.get("Chips",0)
-                value=menu_items.get("Chips",0)
-                value=value*count
-            col2.write(value)
-            if elli_opening_time <= current_time <= elli_closing_time:
-                st.info("Elli is currently open from 12 PM to 9 PM.")
-            else:
-                st.warning("Elli is currently closed. You cannot proceed with your order at this time.")
+            if menu_elli == "Chips":
+                if st.button("Add Item"):
+                    key = "Chips"
+                    value = 2.99
+                    count += 1
+                    if key not in shopping_cart:
+                        shopping_cart[key] = 0
+                    shopping_cart[key] += value
+            st.write(count)
+            st.write(shopping_cart)
             for item in menu_items.keys():
                 price = menu_items[item]
                 col2.write(f"{item}: {price}")
