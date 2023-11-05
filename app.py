1fde4be3c1f1d6400721c51ea2312b9fb72be7ed
Riley Chen <144640158+rileychen7@users.noreply.github.com>
2023-11-04 22:55:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -252,6 +252,12 @@ if st.session_state.page == 'Home Page':
             col2.subheader("Pistachio's")
             col2.write("""Pistachio's is the perfect place to go for delicious pasta dishes from Bravo Pasta or a hot panini from the Bread Box Deli.
     At Bravo Pasta, our culinary team is blending the freshest and finest quality ingredients to make hearty pasta dishes!""")
+
+        elif restaurant_north == "Tims":
+            col1, col2 = st.columns([1, 3])  
+            col1.image("tims.png", use_column_width=True)
+            col2.subheader("Tim Hortons")
+            col2.write("Hot, fresh coffee and delicious baked goods. Serving a variety of muffins, bagels, doughnuts and more!")
             
         elif restaurant_north == "Kali":
             col1, col2 = st.columns([1, 3])  
